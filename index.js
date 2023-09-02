0d2779b74a8f66bf13ce5ac9530c98086eb798b2
Akram-Zreaq <94960537+Akram-Zreaq@users.noreply.github.com>
2023-09-02 21:57:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ app.post("/webhook",(req,res)=>{
 
     if(body_param.object){
         console.log("Inside body param object");
-        if(body_param.entry 
+        if(body_param.entry && 
+            body_param.entry[0].changes 
             ){
                 console.log("Inside body param entry");
                 let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
